484e5165af6076fd197701e9f6bb2e2317aa865d
KumarNeelabhSingh <96595375+KumarNeelabhSingh@users.noreply.github.com>
2024-07-31 07:16:20 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,32 +9,5 @@ resource "aws_vpc" "dep1" {
       "Name" = "VPC1-NTS-18"
       
     }
-    depends_on = [ aws_vpc.dep4 ]
-  
-}
-resource "aws_vpc" "dep2" {
-    cidr_block = "10.20.0.0/16"
-    tags = {
-      "Name" = "VPC2-NTS-18"
-      
-    }
-    depends_on = [ aws_vpc.dep3 ]
-  
-}
-resource "aws_vpc" "dep3" {
-    cidr_block = "10.30.0.0/16"
-    tags = {
-      "Name" = "VPC3-NTS-18"
-      
-    }
-    depends_on = [ aws_vpc.dep4 ]
-  
-}
-resource "aws_vpc" "dep4" {
-    cidr_block = "10.40.0.0/16"
-    tags = {
-      "Name" = "VPC4"-NTS-18
-      
-    }
-  
-}
+   }
+
